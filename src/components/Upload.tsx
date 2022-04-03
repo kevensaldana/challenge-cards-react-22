7e254dbc1a8f5b539b9020d888cb821f213f82ee
keven <keven.sa17@gmail.com>
2022-04-03 12:34:46 +0200

    feat: add image preview in upload component

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import { ChangeEvent, forwardRef, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 export interface UploadFile {
@@ -8,6 +8,7 @@ export interface UploadFile {
 
 interface UploadProps {
   onChange: (e: UploadFile) => void;
+  hasPreview?: boolean;
   name: string;
   id?: string;
 }
@@ -16,22 +17,42 @@ const SCInput = styled.input`
   width: 100%;
 `;
 
-const Upload = React.forwardRef<HTMLInputElement, UploadProps>(
-  ({ onChange, name, id }, ref) => {
+const SCImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+  margin-top: 0.7rem;
+`;
+
+const Upload = forwardRef<HTMLInputElement, UploadProps>(
+  ({ onChange, name, id, hasPreview = true }, ref) => {
+    const [file, setFile] = useState<File | null>(null);
+    const refImage = useRef<HTMLImageElement | null>(null);
+
+    useEffect(() => {
+      if (file && refImage.current) {
+        refImage.current.src = URL.createObjectURL(file);
+      }
+    }, [file]);
+
     return (
-      <SCInput
-        id={id}
-        type="file"
-        name={name}
-        ref={ref}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          onChange &&
-            onChange({
-              file: e.target.files?.length ? e.target.files[0] : null,
-              name,
-            });
-        }}
-      />
+      <>
+        <SCInput
+          id={id}
+          type="file"
+          name={name}
+          ref={ref}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            const file = e.target.files?.length ? e.target.files[0] : null;
+            setFile(file);
+            onChange &&
+              onChange({
+                file,
+                name,
+              });
+          }}
+        />
+        {hasPreview && file && <SCImage ref={refImage} />}
+      </>
     );
   }
 );
